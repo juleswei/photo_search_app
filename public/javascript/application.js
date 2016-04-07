26869189bbd01fc66a95f8739d565f1ca1c62f2f
Jules Wei <s.jules2000@gmail.com>
2016-04-07 05:45:15 +0000

    added ajax request for search in application.js

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
 
+  $('form').submit(function(e) {
+    e.preventDefault();
+    var search = $('#search_type').val();
+    var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var flickrParmas ={ 
+      tags: search,
+      format: "json" 
+    };
+  function callback(data) {
+      var displayPhoto = photoListTemplate({photos:data.items})
+
+      var displayPhoto = '<ul>';
+      $.each(data.items,function(index,items) {
+        displayPhoto += '<li>';
+        displayPhoto += '<img src="'+ items.media.m + '"></li>';     
+      });
+      displayPhoto += '</ul>';
+      $('#photo_collage').html(displayPhoto);
+    };
+
+
+
+    $.getJSON(flickrAPI, flickrParmas, callback);
+
 });
